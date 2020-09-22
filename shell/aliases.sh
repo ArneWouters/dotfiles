f33f0ef92e37cdb716889800c7d782d5715fc04d
Arne Wouters <arne@wouters.co>
2020-09-22 23:24:26 +0200

    add aliases for exa to shell

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,4 +1,3 @@
-
 #lazy stuff
 alias ..="cd .."
 
@@ -8,3 +7,9 @@ alias tmux="TERM=screen-256color-bce tmux"
 # spark stuff
 export SPARK_HOME=/opt/spark
 export PATH=$SPARK_HOME/bin:$PATH
+
+# exa stuff
+alias l='exa -1a'
+alias ll='exa -lh --git'
+alias ls=exa
+alias lt='exa -lT --git'
