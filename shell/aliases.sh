0e4a9bd688a3d51357f071767d510da457331d31
Arne Wouters <awouters@amazon.com>
2024-09-11 13:49:41 +0000

    Add brazil aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -6,3 +6,18 @@ alias l='exa -1a'
 alias ll='exa -lh --git'
 alias ls=exa
 alias lt='exa -lT --git'
+
+# brazil
+alias bb=brazil-build
+alias bba='brazil-build apollo-pkg'
+alias bre='brazil-runtime-exec'
+alias brc='brazil-recursive-cmd'
+alias bws='brazil ws'
+alias bwsuse='bws use -p'
+alias bwscreate='bws create -n'
+alias brc=brazil-recursive-cmd
+alias bbr='brc brazil-build'
+alias bball='brc --allPackages'
+alias bbb='brc --allPackages brazil-build'
+alias bbra='bbr apollo-pkg'
+
